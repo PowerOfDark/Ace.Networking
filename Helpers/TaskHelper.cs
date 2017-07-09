476366fcda6adc408617ebd0ecf550a8fb544c4c
PowerOfDark <przem2003@o2.pl>
2017-07-09 18:22:23 +0200

    Added TaskHelper

---
+++ Helpers/TaskHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ace.Networking.Helpers
+{
+    public static class TaskHelper
+    {
+        public static TaskCompletionSource<T> CreateTaskCompletionSource<T>(object state = null, TimeSpan? timeout = null)
+        {
+            var tcs = new TaskCompletionSource<T>(state);
+            if (timeout.HasValue)
+            {
+                var cts = new CancellationTokenSource(timeout.Value);
+                cts.Token.Register(t =>
+                {
+                    var task = (TaskCompletionSource<T>)t;
+                    task.TrySetCanceled();
+                }, tcs);
+            }
+            return tcs;
+        }
+    }
+}
