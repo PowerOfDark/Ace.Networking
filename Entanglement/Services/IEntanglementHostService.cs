043184fa11a602ab6388e96cb1589fe4d9a2108a
PowerOfDark <przem2003@o2.pl>
2018-07-09 12:25:37 +0200

    Added a comment explaining the inner workings of RegisterAll

--- Entanglement/Services/IEntanglementHostService.cs
+++ Entanglement/Services/IEntanglementHostService.cs
@@ -12,6 +12,12 @@ namespace Ace.Networking.Entanglement.Services
         IEntanglementHostService Register<TBase, T>(EntanglementAccess access) where TBase : class/*, IEntangledObject*/
             where T : EntangledHostedObjectBase, TBase;
 
+        /// <summary>
+        /// Calls <see cref="Register{TBase, T}(EntanglementAccess)"/> for each type (with <see cref="EntanglementAttribute"/> applied) found in the <paramref name="assembly"/>,
+        /// optionally filtering by namespace <paramref name="namespaceBase"/>.
+        /// </summary>
+        /// <param name="namespaceBase">The base namespace; specify <code>null</code> for any</param>
+        /// <param name="assembly">The target assembly to be scanned; specify <code>null</code> to use the entry assembly</param>
         IEntanglementHostService RegisterAll(string namespaceBase = null, Assembly assembly = null);
 
         Guid? GetInstance(Guid interfaceId, IConnection scope = null);
