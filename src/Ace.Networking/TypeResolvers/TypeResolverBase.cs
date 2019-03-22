3684435c38fb2ed4c8951da30f82ea630ee0dc95
PowerOfDark <przem2003@o2.pl>
2019-03-22 01:34:11 +0100

    Update TypeResolverBase.cs

--- src/Ace.Networking/TypeResolvers/TypeResolverBase.cs
+++ src/Ace.Networking/TypeResolvers/TypeResolverBase.cs
@@ -47,8 +47,8 @@ namespace Ace.Networking.TypeResolvers
         public virtual void RegisterType(Type type, TKey key)
         {
             var guid = key;
-            if (Types.TryGetValue(guid, out var t1) && t1 != type ||
-                TypesLookup.TryGetValue(type, out var t2) && !EqualityComparer<TKey>.Default.Equals(t2, guid))
+            if (Types.TryGetValue(guid, out var t1) && t1 != type /*||
+                TypesLookup.TryGetValue(type, out var t2) && !EqualityComparer<TKey>.Default.Equals(t2, guid)*/)
                 throw new ArgumentException(nameof(type));
             Types[guid] = type;
             TypesLookup[type] = guid;
